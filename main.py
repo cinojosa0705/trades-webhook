bb2efbaa6d1077745a823bcf5e1d8e263d913222
Thales <81639334+cinojosa0705@users.noreply.github.com>
2024-02-27 17:23:55 -0500

    comit

---
+++ main.py
@@ -0,0 +1,90 @@
+from typing import Dict, Any
+from flask import Flask, request, jsonify
+from dexteritysdk.dex.sdk_context import SDKContext
+from dexteritysdk.dex.events import OrderFillEvent
+from solders.keypair import Keypair
+from solders.pubkey import Pubkey
+from solana.rpc.api import Client
+
+rpc = "https://devnet-rpc.shyft.to?api_key=JUvCRqsUep-u4yk0"
+client = Client(rpc)
+
+mpg = "BRWNCEzQTm8kvEXHsVVY9jpb1VLbpv9B8mkF43nMLCtu"
+market_product_group_key = Pubkey.from_string(mpg)
+
+payer = Keypair()
+
+ctx = SDKContext.connect(
+	client=client,
+	market_product_group_key=market_product_group_key,
+	payer=payer,
+	raise_on_error=True
+)
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def status():
+    return jsonify({'status': 'live'})
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    if not isinstance(data, list) or not data:
+        return jsonify({'error': "Invalid or missing data"}), 400
+    
+    data = data[0]
+    if data.get("meta", {}).get("err") is not None:
+        return jsonify({'error': "Transaction failed"}), 400
+
+    try:    
+        handle_transaction(data)
+        return jsonify({"message": "Transaction processed"}), 200
+    except Exception as e:
+        # Log the exception for debugging purposes
+        print(f"Exception during transaction processing: {e}")
+        return jsonify({'error': "Transaction failed to process"}), 500
+
+def handle_transaction(tr: Dict[str, Any]):
+    """
+    Checks the given transaction object for fill events and calls the Trading API if found.
+    
+    :param tr: A dictionary representing the transaction.
+    """
+
+    events = ctx.parse_events_from_logs(tr.get("meta", {}).get("logMessages", []))
+    fill_events = [event for event in events if isinstance(event, OrderFillEvent)]
+
+    if fill_events:
+        parsed_trades = [event_to_trade_data(tr, event) for event in fill_events]
+
+        try:
+            for trade in parsed_trades:
+                print(trade)
+            print(f"Sent {len(parsed_trades)} trade events.")
+        except Exception as e:
+            print(f"Failed to send fill events due to error: {e}")
+    else:
+        print("No fill events found in transaction.")
+
+def event_to_trade_data(tr: Dict[str, Any], event: OrderFillEvent) -> Dict[str, Any]:
+    """
+    Parses an OrderFillEvent into a Trade Object.
+    
+    :param tr: The transaction dictionary.
+    :param event: The OrderFillEvent instance.
+    :return: A Trade Object representing the fill event to send to the Trading API for execution.
+    """
+    fill = {
+        "product": event.product,
+        "taker_side": event.taker_side.get_name().lower(),
+        "quote_size": event.quote_size,
+        "base_size": event.base_size,
+        "maker": str(event.maker_trader_risk_group),
+        "taker": str(event.taker_trader_risk_group),
+        "price": event.price,
+    }
+    return fill
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port="5000")
